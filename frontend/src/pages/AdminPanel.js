8ba0c89aa48d113ba5e4069e00bcc2da805cf6c8
Malini Bhattacharya <maliniya30@gmail.com>
2026-05-28 22:07:50 +0530

    Fixed useNavigate import

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { usenavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./AdminPanel.css";
 function AdminPanel() {
