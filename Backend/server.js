197bbaf8cb9665899498ec85a1acb36a6979e812
Malini Bhattacharya <maliniya30@gmail.com>
2026-05-28 20:08:53 +0530

    Updated backend for MongoDB Atlas

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ app.use('/api/residence-certificate',residenceCertificateRoutes );
 
 
 
-const port=5000
+const port= process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
